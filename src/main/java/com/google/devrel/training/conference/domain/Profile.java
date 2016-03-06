44886a6932c2cecb7b117ae46b40cd47dbcb8b4e
Nikolay <kolyan95@gmail.com>
2016-03-06 22:44:07 +0200

    add pr7conference project

---
+++ src/main/java/com/google/devrel/training/conference/domain/Profile.java
@@ -0,0 +1,101 @@
+package com.google.devrel.training.conference.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+import com.google.devrel.training.conference.form.ProfileForm.TeeShirtSize;
+import com.googlecode.objectify.annotation.Cache;
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+
+// TODONE indicate that this class is an Entity
+@Entity
+@Cache
+public class Profile {
+	String displayName;
+	String mainEmail;
+	TeeShirtSize teeShirtSize;
+
+	// TODONE indicate that the userId is to be used in the Entity's key
+	@Id
+	String userId;
+
+	// List of conferences the user has registered to attend
+	private List<String> conferenceKeysToAttend = new ArrayList<>(0);
+
+	
+	/**
+	 * Public constructor for Profile.
+	 * 
+	 * @param userId
+	 *            The user id, obtained from the email
+	 * @param displayName
+	 *            Any string user wants us to display him/her on this system.
+	 * @param mainEmail
+	 *            User's main e-mail address.
+	 * @param teeShirtSize
+	 *            The User's tee shirt size
+	 * 
+	 */
+	public Profile(String userId, String displayName, String mainEmail,
+			TeeShirtSize teeShirtSize) {
+		this.userId = userId;
+		this.displayName = displayName;
+		this.mainEmail = mainEmail;
+		this.teeShirtSize = teeShirtSize;
+	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	public String getMainEmail() {
+		return mainEmail;
+	}
+
+	public TeeShirtSize getTeeShirtSize() {
+		return teeShirtSize;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public List<String> getConferenceKeysToAttend() {
+        return ImmutableList.copyOf(conferenceKeysToAttend);
+    }
+    
+    public void addToConferenceKeysToAttend(String conferenceKey) {
+        conferenceKeysToAttend.add(conferenceKey);
+    }
+    
+    /**
+     * Remove the conferenceId from conferenceIdsToAttend.
+     *
+     * @param conferenceKey a websafe String representation of the Conference Key.
+     */
+    public void unregisterFromConference(String conferenceKey) {
+        if (conferenceKeysToAttend.contains(conferenceKey)) {
+            conferenceKeysToAttend.remove(conferenceKey);
+        } else {
+            throw new IllegalArgumentException("Invalid conferenceKey: " + conferenceKey);
+        }
+    }
+	
+	/**
+	 * Just making the default constructor private.
+	 */
+	@SuppressWarnings("unused")
+	private Profile() {
+	}
+
+	public void update(String displayName2, TeeShirtSize teeShirtSize2) {
+		if (displayName2 != null)
+			displayName = displayName2;
+		if (teeShirtSize2 != null)
+			teeShirtSize = teeShirtSize2;
+	}
+
+}
